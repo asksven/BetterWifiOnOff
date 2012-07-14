c36a05441f323b08b34f103865c7e8272302d706
asksven <sven.knispel@gmail.com>
2012-07-14 23:18:16 +0200

    Added services and handlers

---
+++ BetterWifiOnOff/src/com/asksven/betterwifionoff/BroadcastHandler.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2011 asksven
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.asksven.betterwifionoff;
+
+
+import java.util.logging.Logger;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.os.BatteryManager;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+
+/**
+ * General broadcast handler: handles event as registered on Manifest
+ * @author sven
+ *
+ */
+public class BroadcastHandler extends BroadcastReceiver
+{	
+	private static final String TAG = "BroadcastHandler";
+	
+	/* (non-Javadoc)
+	 * @see android.content.BroadcastReceiver#onReceive(android.content.Context, android.content.Intent)
+	 */
+	@Override
+	public void onReceive(Context context, Intent intent)
+	{
+		SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+
+ 
+        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED))
+		{
+        	// start the service
+//        	context.startService(new Intent(context, BetterBatteryStatsService.class));
+        	
+		}
+
+
+        if (intent.getAction().equals(Intent.ACTION_POWER_DISCONNECTED))
+		{
+			Log.i(TAG, "Received Broadcast ACTION_POWER_DISCONNECTED, seralizing 'since unplugged'");
+			
+			boolean bProcess = sharedPrefs.getBoolean("wifi_off_when_power_ununplug", false);
+			
+			if (!bProcess)
+			{
+				// start service to turn off wifi
+				Intent serviceIntent = new Intent(context, SetWifiStateService.class);
+				serviceIntent.putExtra(SetWifiStateService.EXTRA_STATE, true);
+				context.startService(serviceIntent);
+			}
+		}
+
+
+	}
+}
---
+++ BetterWifiOnOff/src/com/asksven/betterwifionoff/ScreenEventHandler.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (C) 2012 asksven
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.asksven.betterwifionoff;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+import com.asksven.android.common.privateapiproxies.BatteryStatsProxy;
+import com.asksven.android.common.privateapiproxies.Misc;
+import com.asksven.android.common.privateapiproxies.StatElement;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * @author sven
+ *
+ */
+public class ScreenEventHandler extends BroadcastReceiver
+{
+
+	private static final String TAG = "ScreenEventHandler";
+
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+		SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+
+        if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF))
+		{
+			Log.i(TAG, "Received Broadcast ACTION_SCREEN_OFF");
+			boolean bProcess = sharedPrefs.getBoolean("wifi_off_when_screen_off", false);
+			
+			if (!bProcess)
+			{
+				// start service to turn off wifi
+				Intent serviceIntent = new Intent(context, SetWifiStateService.class);
+				serviceIntent.putExtra(SetWifiStateService.EXTRA_STATE, false);
+				context.startService(serviceIntent);
+			}
+		}
+
+        if (intent.getAction().equals(Intent.ACTION_SCREEN_ON))
+		{
+			Log.i(TAG, "Received Broadcast ACTION_SCREEN_ON");
+			boolean bProcess = sharedPrefs.getBoolean("wifi_on_when_screen_on", false);
+			
+			if (!bProcess)
+			{
+				// start service to turn off wifi
+				Intent serviceIntent = new Intent(context, SetWifiStateService.class);
+				serviceIntent.putExtra(SetWifiStateService.EXTRA_STATE, true);
+				context.startService(serviceIntent);
+			}
+
+
+
+			
+		}
+        
+        if (intent.getAction().equals(Intent.ACTION_USER_PRESENT))
+		{
+			Log.i(TAG, "Received Broadcast ACTION_USER_PRESENT");
+			boolean bProcess = sharedPrefs.getBoolean("wifi_on_when_screen_unlock", false);
+			
+			if (!bProcess)
+			{
+				// start service to turn off wifi
+				Intent serviceIntent = new Intent(context, SetWifiStateService.class);
+				serviceIntent.putExtra(SetWifiStateService.EXTRA_STATE, true);
+				context.startService(serviceIntent);
+			}
+			
+
+		}
+
+        Intent i = new Intent(context, EventWatcherService.class);
+        context.startService(i);
+        
+//		// Build the intent to update widgets
+//		Intent intentRefreshWidgets = new Intent(LargeWidgetProvider.WIDGET_UPDATE);
+//		context.sendBroadcast(intentRefreshWidgets);
+
+    }
+    
+}
