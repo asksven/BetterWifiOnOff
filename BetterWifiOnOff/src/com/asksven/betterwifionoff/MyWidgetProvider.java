c5bbe42fc154e1edc698e2da5a5130e91409a079
asksven <sven.knispel@gmail.com>
2012-11-05 22:44:31 +0100

    Added widget (#74)

---
+++ BetterWifiOnOff/src/com/asksven/betterwifionoff/MyWidgetProvider.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2012 asksven
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.asksven.betterwifionoff;
+
+import com.asksven.betterwifionoff.services.UpdateWidgetService;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class MyWidgetProvider extends AppWidgetProvider
+{
+
+	private static final String ACTION_CLICK = "ACTION_CLICK";
+	private static final String TAG = "BetterWifOnOff.MyWidgetProvider";
+
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds)
+	{
+
+		Log.w(TAG, "onUpdate method called");
+		// Get all ids
+		ComponentName thisWidget = new ComponentName(context, MyWidgetProvider.class);
+		int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
+
+		// Build the intent to call the service
+		Intent intent = new Intent(context.getApplicationContext(), UpdateWidgetService.class);
+		intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, allWidgetIds);
+
+		// Update the widgets via the service
+		context.startService(intent);
+	}
+}
---
+++ BetterWifiOnOff/src/com/asksven/betterwifionoff/services/UpdateWidgetService.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (C) 2012 asksven
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.asksven.betterwifionoff.services;
+
+import java.util.Random;
+
+import com.asksven.betterwifionoff.MyWidgetProvider;
+import com.asksven.betterwifionoff.R;
+import com.asksven.betterwifionoff.data.EventLogger;
+
+import android.app.PendingIntent;
+import android.app.Service;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.IBinder;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+public class UpdateWidgetService extends Service
+{
+	private static final String TAG = "BetterWifiOnOff.UpdateWidgetService";
+
+	@Override
+	public void onStart(Intent intent, int startId)
+	{
+		Log.i(TAG, "Called");
+		// Create some random data
+
+		AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this.getApplicationContext());
+
+		int[] allWidgetIds = intent.getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS);
+
+		ComponentName thisWidget = new ComponentName(getApplicationContext(), MyWidgetProvider.class);
+		int[] allWidgetIds2 = appWidgetManager.getAppWidgetIds(thisWidget);
+		Log.w(TAG, "From Intent" + String.valueOf(allWidgetIds.length));
+		Log.w(TAG, "Direct" + String.valueOf(allWidgetIds2.length));
+
+		for (int widgetId : allWidgetIds)
+		{
+			// read the current status
+			SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+    		boolean bDisabled = sharedPrefs.getBoolean("disable_control", false);
+
+    		// write back the new value
+    		boolean bNewState = !bDisabled;
+            SharedPreferences.Editor editor = sharedPrefs.edit();
+            editor.putBoolean("disable_control", bNewState);
+            editor.commit();
+
+            // update widgets
+			RemoteViews remoteViews = new RemoteViews(this.getApplicationContext().getPackageName(),
+					R.layout.widget_layout);
+			// Set the text
+			if (bNewState)
+			{
+				remoteViews.setTextViewText(R.id.status, "Off");
+			}
+			else
+			{
+				remoteViews.setTextViewText(R.id.status, "On");
+			}
+			
+			// Register an onClickListener
+			Intent clickIntent = new Intent(this.getApplicationContext(), MyWidgetProvider.class);
+
+			clickIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+			clickIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, allWidgetIds);
+
+			// make the widget clickable
+			PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, clickIntent,
+					PendingIntent.FLAG_UPDATE_CURRENT);
+			remoteViews.setOnClickPendingIntent(R.id.status, pendingIntent);
+			appWidgetManager.updateAppWidget(widgetId, remoteViews);
+		}
+		stopSelf();
+
+		super.onStart(intent, startId);
+	}
+
+	@Override
+	public IBinder onBind(Intent intent)
+	{
+		return null;
+	}
+}
